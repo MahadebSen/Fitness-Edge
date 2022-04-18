40b9b94db3372f017d537baed5c96bdc1a06b133
Mahadeb Sen <mahadebsen237@gmail.com>
2022-04-18 21:21:08 +0530

    login complete

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,75 @@
-import React from "react";
+import React, { useRef } from "react";
 import google from "../../images/Google.png";
 import github from "../../images/Github.png";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  const [signInWithGithub, githubUser, githubLoading, githubError] =
+    useSignInWithGithub(auth);
+  const [sendPasswordResetEmail, sending, resetError] =
+    useSendPasswordResetEmail(auth);
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    signInWithEmailAndPassword(email, password);
+  };
+
+  let errorMsg;
+  if (error || googleError || githubError || resetError) {
+    errorMsg = (
+      <div>
+        <p className="text-red-600">{error.message}</p>
+      </div>
+    );
+  }
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle();
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithGithub();
+  };
+
+  const handlePasswordReset = () => {
+    const email = emailRef.current.value;
+    if (email) {
+      sendPasswordResetEmail(email);
+      toast("Password reset email sent");
+    }
+  };
+
   return (
     <div className="h-[750px] flex justify-center items-center">
       <div className="px-8 rounded-lg border-2 border-cyan-400 mb-16">
         <p className="text-center mb-9 mt-5 text-3xl font-semibold">Login</p>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="my-3">
             <p className="text-lg my-2">Email</p>
             <input
               className="w-[330px] h-9 border-2 border-blue-500 rounded-lg text-lg px-2"
               type="text"
+              ref={emailRef}
+              required
             />
           </div>
           <div className="my-3">
@@ -21,11 +77,14 @@ const Login = () => {
             <input
               className="w-[330px] h-9 border-2 border-blue-500 rounded-lg text-xl px-2"
               type="password"
+              ref={passwordRef}
+              required
             />
           </div>
+          <div>{errorMsg}</div>
           <div className="flex justify-center mt-6 mb-4">
             <button className="border-2 px-4 py-2 border-red-400 rounded-xl text-lg font-medium hover:bg-red-600 hover:text-white">
-              Submit
+              Login
             </button>
           </div>
         </form>
@@ -38,17 +97,30 @@ const Login = () => {
           </div>
           <div className="flex">
             <p>Don't remember password?</p>
-            <p className="ml-1 text-indigo-600">Forget Password</p>
+            <p onClick={handlePasswordReset} className="ml-1 text-indigo-600">
+              Forget Password
+            </p>
           </div>
         </div>
         <div className="flex justify-center">
           <p className="font-medium">Or, login using</p>
         </div>
         <div className="flex justify-center mb-5">
-          <img className="w-[65px] my-3 mx-2" src={google} alt="" />
-          <img className="w-[65px] my-3 mx-2" src={github} alt="" />
+          <img
+            onClick={handleGoogleSignIn}
+            className="w-[65px] my-3 mx-2"
+            src={google}
+            alt=""
+          />
+          <img
+            onClick={handleGithubSignIn}
+            className="w-[65px] my-3 mx-2"
+            src={github}
+            alt=""
+          />
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
