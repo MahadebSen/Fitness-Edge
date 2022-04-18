3cb722bf83023513000a69fcb28ec9c7427dea95
Mahadeb Sen <mahadebsen237@gmail.com>
2022-04-18 23:10:23 +0530

    readme edited

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Getting Started with Create React App
+# Fitness-Edge
+
+#Live site: 'https://fitness-edge-4e65f.web.app/'
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,22 @@
 import React from "react";
+import me from "../../images/me.jpg";
 
 const About = () => {
   return (
-    <div>
-      <h1>This is about me</h1>
+    <div className="flex justify-center items-center h-[750px]">
+      <div className="text-center font-semibold text-xl">
+        <img className="w-[250px] mx-auto" src={me} alt="" />
+        <p className="my-5">Hi, my name is Mahadeb Sen.</p>
+        <p className="my-5">
+          In short, I'm passionate about programming. And I love to design web
+          site. So that's why I decided to be a Full stack web developer.{" "}
+        </p>
+        <p className="my-5">
+          To fulfil my desire or, to achieving my goals, I have joined
+          Programming hero bootcamp. Also I'm doing a course in
+          freecodecamp.org.
+        </p>
+      </div>
     </div>
   );
 };
